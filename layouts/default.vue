6e3c4e0dde8841acb37525271d2e8d60abb1b4df
Рамиль Хаматшин <ramil_kh@Ramils-MacBook-Air.local>
2025-03-30 13:34:54 +0500

    feat(ui): added deafult layout and implement it in app.vue

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import AppHeader from "@/components/common/AppHeader.vue";
+</script>
+
+<template>
+  <div class="layout">
+    <AppHeader />
+    <main class="layout__main">
+      <slot />
+    </main>
+  </div>
+</template>
+
+<style>
+.layout {
+  min-height: 100vh;
+  overflow-x: hidden;
+}
+
+.layout__main {
+  width: 100%;
+  max-width: 100vw;
+  padding-top: 241px;
+}
+</style>
