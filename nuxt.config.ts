c0e01b40a9923ae00ef11ee96f3972ffe8198dd6
Рамиль Хаматшин <ramil_kh@Ramils-MacBook-Air.local>
2025-03-31 02:43:26 +0500

    chore: added favicon config to nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,7 @@ export default defineNuxtConfig({
       },
       charset: "utf-8",
       viewport: "width=device-width, initial-scale=1",
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.svg" }],
     },
   },
 });
