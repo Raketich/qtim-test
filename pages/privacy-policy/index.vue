4b6133b4012a0d49db1f0830c5f05fd0cbf8b0e9
Рамиль Хаматшин <ramil_kh@Ramils-MacBook-Air.local>
2025-03-31 10:53:03 +0500

    feat: added privacy policy page

---
+++ pages/privacy-policy/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <h1>Some boring information we need to tell you about Privacy Policy</h1>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  max-width: 1216px;
+  height: 50vh;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+}
+</style>
